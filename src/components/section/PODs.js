776d1e6af7a560c3e9e02f55664de2b641492ac5
Your Name <you@example.com>
2022-01-13 10:18:58 -0700

    dash game demo

--- src/components/section/PODs.js
+++ src/components/section/PODs.js
@@ -8,50 +8,7 @@ export function PODs() {
             <div className="title">
                 <div className='cool-text'>METAVERSE</div>
             </div>
-            <div className="row">
-                <div className='col' style={{marginTop:'100px', marginLeft:'0px', display:'block'}}>
-                    <div className="game-card">
-                        <img src="images/VR/render.gif" alt="City" className="game-image" />
-                        <div className="game-text">
-                            OWN
-                        </div>
-                    </div>
-                    <div className="game-card">
-                        <img src="images/VR/VR.jpg" alt="City" className="game-image" />
-                        <div className="game-text">
-                            SHARE
-                        </div>
-                    </div>
-                    <div className="game-card">
-                        <img src="images/VR/dot_world.gif" alt="City" className="game-image" />
-                        <div className="game-text">
-                            EXPLORE
-                        </div>
-                    </div>
-                </div>
-                <div className='col'>
-                    <p className='bottom-space' />
-                    <h3 className="neon" style={{fontSize: '25px', fontFamily: 'neon', textAlign:'end'}}>
-                        DAO Estates
-                    </h3>
-                    <h3 style={{fontSize: '15px', textAlign: 'justify'}}>
-                        Spaces are not stagnant, they modify and adapt with time. We live in a place in time where the dialog is turning into a monologue,
-                        people are loosing access or the feeling of local scale community. There are somethings we can't change but we can adapt as well.
-                        Our aim is to reestablish common spaces in collective habbitats.
-                        Our designers began adapting their real-life solutions into the digital space of Virtual Reality and
-                        build common spaces for all cosmosapien investors.
-                        Exclusive worlds as well as custom private spaces available 
-                        to all Cosmosapiens as ERC-721 tokens on the Ethereum blockchain and hosted on IPFS,
-                        forever yours to owns and use or share.
-                        
-                    </h3>
-                    <p className="bottom-space-sm" />
-                    <h3 style={{fontSize: '15px', textAlign: 'justify'}}>
-                        Connect with one our design team members if you're interested in developing a custom
-                        designed space for yourself. 
-                    </h3>
-                </div>
-            </div>
+            
             <div className="row">
                 <div className='col'>
                 <p className='bottom-space-sm' />
@@ -104,6 +61,67 @@ export function PODs() {
                             VICI
                         </div>
                     </div>
+                </div>
+            </div>
+            <div className="row" style={{marginTop:'0px'}}>
+                <div className='col'>
+                <h3 style={{fontSize: '18px', textAlign: 'center', marginBottom: '-50px'}}>TRY OUT OUR DEMO </h3>
+                    <h3 className="neon" style={{fontSize: '35px', fontFamily: 'neon', textAlign:'center'}}>
+                        COMETA DASH
+                    </h3>
+                    <p className="bottom-space-sm" />
+                    <a href='http://dash.cometaclub.com/'>
+
+                    <div className='img-zoom'>
+                          <img src='images/web/dash.png'  width="350px" alt=''  />
+                        </div>
+                        </a>
+
+
+                </div>
+            </div>
+            <div className="row">
+                <div className='col' style={{marginTop:'100px', marginLeft:'0px', display:'block'}}>
+                    <div className="game-card">
+                        <img src="images/VR/render.gif" alt="City" className="game-image" />
+                        <div className="game-text">
+                            OWN
+                        </div>
+                    </div>
+                    <div className="game-card">
+                        <img src="images/VR/VR.jpg" alt="City" className="game-image" />
+                        <div className="game-text">
+                            SHARE
+                        </div>
+                    </div>
+                    <div className="game-card">
+                        <img src="images/VR/dot_world.gif" alt="City" className="game-image" />
+                        <div className="game-text">
+                            EXPLORE
+                        </div>
+                    </div>
+                </div>
+                <div className='col'>
+                    <p className='bottom-space' />
+                    <h3 className="neon" style={{fontSize: '25px', fontFamily: 'neon', textAlign:'end'}}>
+                        DAO Estates
+                    </h3>
+                    <h3 style={{fontSize: '15px', textAlign: 'justify'}}>
+                        Spaces are not stagnant, they modify and adapt with time. We live in a place in time where the dialog is turning into a monologue,
+                        people are loosing access or the feeling of local scale community. There are somethings we can't change but we can adapt as well.
+                        Our aim is to reestablish common spaces in collective habbitats.
+                        Our designers began adapting their real-life solutions into the digital space of Virtual Reality and
+                        build common spaces for all cosmosapien investors.
+                        Exclusive worlds as well as custom private spaces available 
+                        to all Cosmosapiens as ERC-721 tokens on the Ethereum blockchain and hosted on IPFS,
+                        forever yours to owns and use or share.
+                        
+                    </h3>
+                    <p className="bottom-space-sm" />
+                    <h3 style={{fontSize: '15px', textAlign: 'justify'}}>
+                        Connect with one our design team members if you're interested in developing a custom
+                        designed space for yourself. 
+                    </h3>
                 </div>
             </div>
             
